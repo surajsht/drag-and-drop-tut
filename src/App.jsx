9143c87eb04a21930a2b6ee11ad50cfdb6ebf69f
suraj <surajsht2055@gmail.com>
2024-06-04 20:59:19 +0545

    fixed ui issue while dragging item

--- src/App.jsx
+++ src/App.jsx
@@ -58,6 +58,7 @@ const App = () => {
                     </Draggable>
                   );
                 })}
+                {provided.placeholder}
               </div>
             )}
           </Droppable>
