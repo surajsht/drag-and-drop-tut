408579cd839761f3ebf9eafe7ffa44b7f8f81c7f
suraj <surajsht2055@gmail.com>
2024-06-04 20:57:01 +0545

    created function to handle drag and drop

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,32 @@ import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 const App = () => {
   let [stores, setStores] = useState(DATA);
 
+  let handleDragDrop = (results) => {
+    let { source, destination, type } = results;
+
+    if (!destination) return;
+
+    if (
+      source.droppableId === destination.droppableId &&
+      source.index === destination.index
+    )
+      return;
+
+    if (type === "group") {
+      let reorderedStores = [...stores];
+      let sourceIndex = source.index;
+      let destinationIndex = destination.index;
+
+      let [removedStore] = reorderedStores.splice(sourceIndex, 1);
+      reorderedStores.splice(destinationIndex, 0, removedStore);
+      return setStores(reorderedStores);
+    }
+  };
+
   return (
     <div className="layout__wrapper">
       <div className="card">
-        <DragDropContext
-          onDragEnd={() => {
-            console.log("drag drop event occured");
-          }}
-        >
+        <DragDropContext onDragEnd={handleDragDrop}>
           <div className="header">
             <h1>Shopping List</h1>
           </div>
