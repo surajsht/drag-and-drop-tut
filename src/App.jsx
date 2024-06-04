2fac6842ffef61591b4326069584522637c32801
suraj <surajsht2055@gmail.com>
2024-06-04 19:57:13 +0545

    initial data rendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import { DATA } from "./Data";
+
 const App = () => {
-  return <div>App</div>;
+  let [stores, setStores] = useState(DATA);
+
+  return (
+    <div className="layout__wrapper">
+      <div className="card">
+        <div className="header">
+          <h1>Shopping List</h1>
+        </div>
+
+        <div>
+          {stores.map((store, idx) => {
+            return (
+              <div key={idx}>
+                <div className="store-container">
+                  <h3> {store.name} </h3>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
